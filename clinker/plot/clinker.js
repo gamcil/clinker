5c57d1e9c3e5087e43db2b99269c5815e935f368
Cameron Gilchrist <clmgilchrist@gmail.com>
2020-10-20 13:19:35 +0800

    add plot folder

---
+++ clinker/plot/clinker.js
@@ -0,0 +1,137 @@
+function serialise(svg) {
+	/* Saves the figure to SVG in its current state.
+	 * Clones the provided SVG and sets the width/height of the clone to the
+	 * bounding box of the original SVG. Thus, downloaded figures will be sized
+	 * correctly.
+	 * This function returns a new Blob, which can then be downloaded.
+	*/
+	node = svg.node();
+	const xmlns = "http://www.w3.org/2000/xmlns/";
+	const xlinkns = "http://www.w3.org/1999/xlink";
+	const xhtml = "http://www.w3.org/1999/xhtml";
+	const svgns = "http://www.w3.org/2000/node";
+	const bbox = svg.select("g").node().getBBox()
+
+	node = node.cloneNode(true);
+	node.setAttribute("width", bbox.width);
+	node.setAttribute("height", bbox.height);
+	node.setAttributeNS(xmlns, "xmlns", svgns);
+	node.setAttributeNS(xmlns, "xmlns:xlink", xlinkns);
+	node.setAttributeNS(xmlns, "xmlns:xhtml", xhtml);
+
+	// Adjust x/y of <g> to account for axis/title position.
+	// Replaces the transform attribute, so drag/zoom is ignored.
+	d3.select(node)
+		.select("g")
+		.attr("transform", `translate(${Math.abs(bbox.x)}, ${Math.abs(bbox.y)})`)
+
+	const serializer = new window.XMLSerializer;
+	const string = serializer.serializeToString(node);
+	return new Blob([string], {type: "image/node+xml"});
+}
+
+function download(blob, filename) {
+	/* Downloads a given blob to filename.
+	 * This function appends a new anchor to the document, which points to the
+	 * supplied blob. The anchor.click() method is called to trigger the download,
+	 * then the anchor is removed.
+	*/
+	const link = document.createElement("a");
+	link.href = URL.createObjectURL(blob);
+	link.download = filename;
+	document.body.appendChild(link);
+	link.click();
+	document.body.removeChild(link);
+}
+
+function plot(data) {
+  const div = d3.select("#plot")
+  const chart = ClusterMap.ClusterMap()
+    .config({
+      scaleFactor: 30, 
+      cluster: {
+        spacing: 50,
+        alignLabels: true,
+      },
+      gene: {
+        label: {
+          show: false,
+        }
+      }
+    })
+
+  let plot = d3.select("#plot")
+    .datum(data)
+    .call(chart)
+
+  let svg = div.select("svg")
+  d3.select("#btn-save-svg")
+    .on("click", () => {
+      const blob = serialise(svg)
+      download(blob, "clinker.svg")
+    })
+
+  function update(opts) {
+    chart.config(opts) 
+    plot.call(chart)
+  }
+
+  // Figure layout
+  d3.select("#input-scale-factor")
+    .on("change", function() {update({plot: {scaleFactor: +this.value}})})
+  d3.select("#input-cluster-spacing")
+    .on("change", function() {update({cluster: {spacing: +this.value}})})
+
+  // Cluster
+  d3.select("#input-cluster-align-labels")
+    .on("change", function() {update({cluster: {alignLabels: d3.select(this).property("checked")}})})
+  d3.select("#input-cluster-name-size")
+    .on("change", function() {update({cluster: {nameFontSize: +this.value}})})
+  d3.select("#input-locus-name-size")
+    .on("change", function() {update({cluster: {lociFontSize: +this.value}})})
+  d3.select("#input-locus-spacing")
+    .on("change", function() {update({locus: {spacing: +this.value}})})
+
+  // Gene polygon shape
+  d3.select("#input-body-height")
+    .on("change", function() {update({gene: {shape: {bodyHeight: +this.value}}})})
+  d3.select("#input-tip-height")
+    .on("change", function() {update({gene: {shape: {tipHeight: +this.value}}})})
+  d3.select("#input-tip-length")
+    .on("change", function() {update({gene: {shape: {tipLength: +this.value}}})})
+
+  // Gene labels
+  d3.select("#input-gene-labels")
+    .on("change", function() {update({gene: {label: {show: d3.select(this).property("checked")}}})})
+  d3.select("#input-label-rotation")
+    .on("change", function() {update({gene: {label: {rotation: +this.value}}})})
+  d3.select("#input-label-start")
+    .on("change", function() {update({gene: {label: {start: +this.value}}})})
+  d3.select("#select-label-anchor")
+    .on("change", function() {update({gene: {label: {anchor: this.value}}})})
+  d3.select("#input-label-size")
+    .on("change", function() {update({gene: {label: {fontSize: +this.value}}})})
+
+  // Scale bar
+  d3.select("#input-scalebar-fontsize")
+    .on("change", function() {update({scaleBar: {fontSize: +this.value}})})
+  d3.select("#input-scalebar-height")
+    .on("change", function() {update({scaleBar: {height: +this.value}})})
+  d3.select("#input-scalebar-basepair")
+    .on("change", function() {update({scaleBar: {basePair: +this.value}})})
+  
+  // Colour bar
+  d3.select("#input-colourbar-fontsize")
+    .on("change", function() {update({colourBar: {fontSize: +this.value}})})
+  d3.select("#input-colourbar-height")
+    .on("change", function() {update({colourBar: {height: +this.value}})})
+
+  // Legend
+  d3.select("#input-legend-fontsize")
+    .on("change", function() {update({legend: {fontSize: +this.value}})})
+  d3.select("#input-legend-entryheight")
+    .on("change", function() {update({legend: {entryHeight: +this.value}})})
+}
+
+d3.json("data.json")
+  .then(plot)
