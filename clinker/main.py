0b68c05bafd9296e00ccf93a4277d17133be1827
Cameron Gilchrist <clmgilchrist@gmail.com>
2020-10-20 13:37:34 +0800

    add main.py and plot.py

---
+++ clinker/main.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+
+"""
+Command line interface
+
+Cameron Gilchrist
+"""
+
+import argparse
+import logging
+
+from pathlib import Path
+
+from clinker import align, plot
+from clinker.classes import parse_files
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] %(levelname)s - %(message)s",
+    datefmt="%H:%M:%S"
+)
+LOG = logging.getLogger(__name__)
+
+
+def clinker(
+    files,
+    identity=0.3,
+    delimiter=None,
+    decimals=2,
+    output=None,
+    force=False,
+    hide_link_headers=False,
+    hide_alignment_headers=False,
+):
+    """Entry point for running the script."""
+    LOG.info("Starting clinker")
+
+    # Check output file before doing anything else
+    if output and Path(output).exists() and not force:
+        LOG.error(f"File {output} already exists but --force not specified, exiting")
+        return
+
+    # Parse files, generate objects
+    LOG.info("Parsing GenBank files")
+    clusters = parse_files(files)
+
+    # Align all clusters
+    LOG.info("Aligning your clusters")
+    globaligner = align.align_clusters(*clusters, cutoff=identity)
+
+    # Generate results summary
+    summary = globaligner.format(
+        delimiter=delimiter,
+        decimals=decimals,
+        link_headers=not hide_link_headers,
+        alignment_headers=not hide_alignment_headers,
+    )
+    if output:
+        LOG.info(f"Writing alignments to {output}")
+        with open(output, "w") as fp:
+            fp.write(summary)
+    else:
+        print(summary)
+
+    # Generate the SVG
+    LOG.info("Plotting alignment...")
+    plot.plot_clusters(globaligner)
+
+    LOG.info("Done!")
+    return globaligner
+
+
+def get_parser():
+    """Creates an ArgumentParser object."""
+    parser = argparse.ArgumentParser(
+        "clinker",
+        description="clinker: Automatic creation of publication-ready"
+        " gene cluster comparison figures.\n\n"
+        "clinker generates gene cluster comparison figures from GenBank files."
+        " It performs pairwise local or global alignments between every sequence"
+        " in every unique pair of clusters and generates interactive, to-scale comparison figures"
+        " using the clustermap.js library.",
+        epilog="Example usage\n-------------\n"
+        "Align clusters, plot results and save scores to file:\n"
+        "  $ clinker files/*.gbk -p -o results.txt\n\n"
+        "Cameron Gilchrist, 2020",
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument("files", help="Gene cluster GenBank files", nargs="+")
+
+    alignment = parser.add_argument_group("Alignment options")
+    alignment.add_argument(
+        "-i",
+        "--identity",
+        help="Minimum alignment sequence identity",
+        type=float,
+        default=0.3
+    )
+
+    output = parser.add_argument_group("Output options")
+    output.add_argument("-f", "--force", help="Overwrite previous output file", action="store_true")
+    output.add_argument("-o", "--output", help="Save alignments to file")
+    output.add_argument("-dl", "--delimiter", help="Character to delimit output by")
+    output.add_argument("-dc", "--decimals", help="Number of decimal places in output", default=2)
+    output.add_argument(
+        "-hl",
+        "--hide_link_headers",
+        help="Hide alignment column headers",
+        action="store_true",
+    )
+    output.add_argument(
+        "-ha",
+        "--hide_aln_headers",
+        help="Hide alignment cluster name headers",
+        action="store_true",
+    )
+
+    return parser
+
+
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+    clinker(
+        args.files,
+        identity=args.identity,
+        delimiter=args.delimiter,
+        decimals=args.decimals,
+        output=args.output,
+        force=args.force,
+        hide_link_headers=args.hide_link_headers,
+        hide_alignment_headers=args.hide_aln_headers,
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ clinker/plot.py
@@ -0,0 +1,114 @@
+import http.server
+import socketserver
+import webbrowser
+import json
+import shutil
+import pathlib
+import logging
+
+from functools import partial
+
+
+LOG = logging.getLogger(__name__)
+
+
+class CustomHandler(http.server.BaseHTTPRequestHandler):
+    """Handler for serving cblaster plots."""
+
+    def __init__(self, data, *args, **kwargs):
+        self._data = data
+        self._dir = pathlib.Path(__file__).resolve().parent.parent / "clinker" / "plot"
+        super().__init__(*args, **kwargs)
+
+    def copy_file(self, source):
+        shutil.copyfileobj(source, self.wfile)
+
+    def send_headers(self, mime):
+        self.send_response(200)
+        self.send_header("Content-Type", mime)
+        self.end_headers()
+
+    def log_message(self, format, *args):
+        """Suppresses logging messages on every request."""
+        return
+
+    def do_GET(self):
+        """Serves each component of the cblaster plot."""
+        if self.path == "/data.json":
+            self.send_headers("text/json")
+            self.wfile.write(json.dumps(self._data).encode())
+            return
+        path, mime = None, None
+        if self.path == "/":
+            path, mime = self._dir / "index.html", "text/html"
+        elif self.path == "/style.css":
+            path, mime = self._dir / "style.css", "text/css"
+        elif self.path == "/d3.min.js":
+            path, mime = self._dir / "d3.min.js", "text/javascript"
+        elif self.path == "/clustermap.min.js":
+            path, mime = self._dir / "clustermap.min.js", "text/javascript"
+        elif self.path == "/clinker.js":
+            path, mime = self._dir / "clinker.js", "text/javascript"
+        if not path:
+            return
+        with path.open("rb") as fp:
+            self.send_headers(mime)
+            self.copy_file(fp)
+
+
+def serve_html(data):
+    """Serve a synthaser plot using the socketserver module."""
+    handler = partial(CustomHandler, data)
+
+    # Instantiate a new server, bind to any open port
+    with socketserver.TCPServer(("localhost", 0), handler) as httpd:
+
+        # Automatically open web browser to bound address
+        address, port = httpd.server_address
+        url = f"http://{address}:{port}/"
+        webbrowser.open(url)
+
+        # Start serving the plot; shutdown on a keyboard interrupt
+        try:
+            LOG.info(f"Serving clinker plot at {url} (Ctrl+C to stop).")
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            httpd.shutdown()
+
+
+def save_html(data, output):
+    """Generates a static HTML file with all visualisation code."""
+
+    directory = pathlib.Path(__file__).resolve().parent.parent / "clinker" / "plot"
+
+    with (directory / "index.html").open() as fp:
+        html = fp.read()
+
+    css_string = '<link href="style.css" rel="stylesheet"></link>'
+    d3_string = '<script src="d3.min.js"></script>'
+    cl_string = '<script src="clinker.js"></script>'
+
+    with (directory / "index.css").open() as fp:
+        css = fp.read()
+        html = html.replace(css_string, f"<style>{css}</style>")
+
+    with (directory / "d3.min.js").open() as fp:
+        d3 = fp.read()
+        html = html.replace(d3_string, f"<script>{d3}</script>")
+
+    with (directory / "clinker.js").open() as fp:
+        cl = f"const data={json.dumps(data)}" + fp.read()
+        html = html.replace(cl_string, f"<script>{cl}</script>")
+
+    with open(output, "w") as fp:
+        fp.write(html)
+
+
+def plot_clusters(clusters, output=None):
+    """Generates clinker plot from a collection of Synthase objects."""
+    data = clusters.to_data()
+    if output:
+        save_html(data, output)
+        webbrowser.open(output)
+    else:
+        serve_html(data)
