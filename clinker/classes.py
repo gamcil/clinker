9dc68ffa85bfef41396898693664934faad742e6
Cameron Gilchrist <clmgilchrist@gmail.com>
2020-12-10 17:46:11 +0800

    Add GFF3+FASTA parser (#24)
    * initial GFF parsing using gffutils
    
    * add gff parsing to main workflow
    
    * add gffutils as dependency
    
    * initial GFF parsing using gffutils
    
    * add gff parsing to main workflow
    
    * add gffutils as dependency
    
    * add None default to translation qualifiers pop
    
    * update gff3 parser: account for non-relative feature locations, get real coordinates
    
    * add CDS->gene traceback for real coordinates to GenBank parser
    
    * cleanup function
    
    * update clustermap
    
    * move line down

--- clinker/classes.py
+++ clinker/classes.py
@@ -7,15 +7,133 @@ Cameron Gilchrist
 """
 
 import json
+import logging
 import warnings
 import uuid
 
 from pathlib import Path
 
+import gffutils
+from gffutils import biopython_integration
+
 from Bio import SeqIO, SeqRecord, BiopythonParserWarning
+from Bio.SeqFeature import FeatureLocation
 
 # ignore malformed locus warnings
 warnings.simplefilter('ignore', BiopythonParserWarning)
+
+LOG = logging.getLogger(__name__)
+
+FASTA_SUFFIXES = (".fa", ".fsa", ".fna", ".fasta", ".faa")
+GBK_SUFFIXES = (".gbk", ".gb", ".genbank", ".gbf", ".gbff")
+GFF_SUFFIXES = (".gtf", ".gff", ".gff3")
+
+
+def find_fasta(gff_path):
+    path = None
+    for suffix in FASTA_SUFFIXES:
+        _path = Path(gff_path).with_suffix(suffix)
+        if _path.exists():
+            path = _path
+            break
+    return path
+
+
+def parse_fasta(path):
+    with open(path) as fp:
+        records = list(SeqIO.parse(fp, "fasta"))
+    return records
+
+
+def find_regions(directives):
+    """Looks for ##sequence-region directives in a list of GFF3 directives."""
+    regions = {}
+    for directive in directives:
+        if directive.startswith("sequence-region"):
+            _, accession, start, end = directive.split(" ")
+            regions[accession] = (int(start), int(end))
+    return regions
+
+
+def parse_gff(path):
+    """Parses a GFF3 file using GFFUtils."""
+
+    # Check for FASTA file
+    fasta_path = find_fasta(path)
+    if not fasta_path:
+        raise FileNotFoundError("Could not find matching FASTA file")
+
+    # Parse FASTA and create GFFUtils database
+    fasta = parse_fasta(fasta_path)
+    gff = gffutils.create_db(
+        str(path),
+        ":memory:",
+        force=True,
+        merge_strategy="create_unique",
+        sort_attribute_values=True
+    )
+    regions = find_regions(gff.directives)
+
+    # Find features for each record in the FASTA file
+    loci = []
+    for record in fasta:
+        try:
+            record_start, _ = regions[record.id]
+        except KeyError:
+            record_start = 1
+        features = list(gff.region(seqid=record.id, featuretype="CDS"))
+        features.sort(key=lambda f: f.start)
+        if not features:
+            raise ValueError(f"Found no CDS features in {record.id} [{path}]")
+
+        previous = None
+        for feature in features:
+            # Check if this feature is part of the previous one for merging
+            seqid = feature.attributes["ID"][0]
+            same_feature = previous == seqid
+            if not previous:
+                previous = seqid
+
+            # Normalise Feature location based on ##sequence-region directive.
+            # Necessary for extracted GFF3 files that still store coordinates
+            # relative to the entire region. If no sequence-region directive
+            # is found, assumes 1 (i.e. default sequence start).
+            feature = biopython_integration.to_seqfeature(feature)
+            feature.location = FeatureLocation(
+                feature.location.start - record_start - 1,
+                feature.location.end - record_start,
+                strand=feature.location.strand
+            )
+
+            # Either merge with previous feature, or append it
+            if same_feature:
+                record.features[-1].location += feature.location
+            else:
+                record.features.append(feature)
+                previous = seqid
+
+        # Try to trace back from CDS to parent gene feature for actual
+        # gene coordinates. If not found (e.g. malformed GFF without ID= and parent=
+        # features), warns user and defaults to CDS start/end.
+        genes = []
+        for feature in record.features:
+            parent = list(gff.parents(gff[feature.id], featuretype="gene"))
+            start, end = None, None
+            if parent:
+                start = parent[0].start - record_start - 1
+                end = parent[0].end - record_start
+            else:
+                LOG.warning(
+                    f"Could not find parent gene of {feature.id}."
+                    " Using coding sequence coordinates instead."
+                )
+            gene = Gene.from_seqfeature(feature, record, start=start, end=end)
+            genes.append(gene)
+
+        locus = Locus(record.id, genes, 0, len(record))
+        loci.append(locus)
+
+    return Cluster(Path(path).stem, loci)
 
 
 def find_qualifier(valid_values, qualifiers):
@@ -56,19 +174,29 @@ def parse_genbank(path):
 def find_files(paths, recurse=True, level=0):
     files = []
     for path in paths:
-        if Path(path).is_dir():
+        _path = Path(path)
+        if _path.is_dir():
             if level == 0 or recurse:
                 new = Path(path).glob("*")
                 _files = find_files(new, recurse=recurse, level=level + 1)
                 files.extend(_files)
         else:
-            if Path(path).exists():
+            if _path.exists() and _path.suffix.lower() in GBK_SUFFIXES + GFF_SUFFIXES:
                 files.append(path)
     return files
 
 
 def parse_files(paths):
-    return [parse_genbank(path) for path in paths]
+    clusters = []
+    for path in paths:
+        if Path(path).suffix.lower() in GBK_SUFFIXES:
+            cluster = parse_genbank(path)
+        elif Path(path).suffix.lower() in GFF_SUFFIXES:
+            cluster = parse_gff(path)
+        else:
+            raise TypeError("File %s does not have GenBank or GFF3 extension")
+        clusters.append(cluster)
+    return clusters
 
 
 def get_children(children, uids_only=False):
@@ -88,6 +216,15 @@ def load_child(child, thing):
 
 def load_children(children, thing):
     return [load_child(child, thing) for child in children]
+
+
+def find_overlapping_location(feature, locations):
+    for location in locations:
+        if (
+            feature.location.start >= location.start
+            and feature.location.end <= location.end
+        ):
+            return location
 
 
 class Serializer:
@@ -136,7 +273,7 @@ class Cluster(Serializer):
     def __init__(self, name, loci, uid=None):
         self.uid = uid if uid else str(uuid.uuid4())
         self.name = name
-        self.loci = loci
+        self.loci = loci if loci else []
 
     def to_dict(self, uids_only=False):
         return {
@@ -159,7 +296,6 @@ class Cluster(Serializer):
 
         Arguments:
             args (SeqRecord): BioPython SeqRecord object/s
-            mode (str): which sequence to extract ('protein' or 'gene')
         """
         loci = [Locus.from_seqrecord(record) for record in args]
         return cls(name=name if name else loci[0].name, loci=loci)
@@ -208,12 +344,27 @@ class Locus(Serializer):
         """Builds a new Locus from a BioPython SeqRecord."""
         if not isinstance(record, SeqRecord.SeqRecord):
             raise NotImplementedError('Supplied argument is not a valid SeqRecord object')
-        genes = [
-            Gene.from_seqfeature(feature, record)
-            for feature in record.features
-            if feature.type == "CDS"
-        ]
-        genes = [gene for gene in genes if gene]
+
+        # Find all CDS SeqFeature and gene FeatureLocations
+        features = [f for f in record.features if f.type == "CDS"]
+        locations = [f.location for f in record.features if f.type == "gene"]
+
+        # Trace CDS features back to genes for real locations, create Gene objects
+        genes = []
+        for feature in features:
+            match = find_overlapping_location(feature, locations)
+            if not match:
+                LOG.warning(
+                    f"Could not find parent gene of {feature.id}."
+                    " Using coding sequence coordinates instead."
+                )
+            gene = Gene.from_seqfeature(
+                feature,
+                record,
+                start=match.start if match else None,
+                end=match.end if match else None,
+            )
+            genes.append(gene)
         return cls(name=record.name, start=0, end=len(record), genes=genes)
 
     def get_gene(self, name):
@@ -268,7 +419,7 @@ class Gene(Serializer):
         return [(key, *self.smcog[key]) for key in ordered]
 
     @classmethod
-    def from_seqfeature(cls, feature, record):
+    def from_seqfeature(cls, feature, record, start=None, end=None):
         """Builds a new Gene object from a BioPython SeqFeature.
 
         Parameters:
@@ -283,13 +434,13 @@ class Gene(Serializer):
             for k, v in feature.qualifiers.items()
         }
         sequence = feature.extract(record.seq)
-        translation = qualifiers.pop("translation") or sequence.translate()
+        translation = qualifiers.pop("translation", None) or sequence.translate()
         return cls(
             names=qualifiers,
             label=get_value(qualifiers, tags),
             sequence=str(sequence),
             translation=str(translation),
-            start=int(feature.location.start),
-            end=int(feature.location.end),
+            start=start if start else int(feature.location.start),
+            end=end if end else int(feature.location.end),
             strand=feature.location.strand,
         )
